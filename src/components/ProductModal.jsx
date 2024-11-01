27d3adc159446ef438c42a27c1bd2867d18c0b02
Bayzid123 <bayzid18103384@gmail.com>
2024-11-01 15:01:45 +0600

    updated

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,72 +1,81 @@
 /* eslint-disable react/prop-types */
 import Modal from "react-modal";
 
-const ProductModal = ({ product, onClose }) => (
-  <Modal
-    isOpen
-    onRequestClose={onClose}
-    ariaHideApp={false}
-    style={{
-      overlay: {
-        zIndex: 1000,
-        backgroundColor: "rgba(0, 0, 0, 0.75)",
-      },
-      content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        width: "80%",
-        maxWidth: "500px",
-        maxHeight: "80%",
-        padding: "20px",
-        borderRadius: "10px",
-        textAlign: "center",
-        overflow: "auto",
-        fontFamily: "'Murecho', sans-serif",
-      },
-    }}
-  >
-    <div
+const ProductModal = ({ product, onClose }) => {
+  const oldPrice = product.discountPercentage
+    ? (product.price / (1 - product.discountPercentage / 100)).toFixed(2)
+    : null;
+
+  return (
+    <Modal
+      isOpen
+      onRequestClose={onClose}
+      ariaHideApp={false}
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#f0f0f0",
-        borderRadius: "10px",
-        padding: "10px",
+        overlay: {
+          zIndex: 1000,
+          backgroundColor: "rgba(0, 0, 0, 0.75)",
+        },
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          width: "80%",
+          maxWidth: "500px",
+          maxHeight: "80%",
+          padding: "20px",
+          borderRadius: "10px",
+          textAlign: "center",
+          overflow: "auto",
+          fontFamily: "'Murecho', sans-serif",
+        },
       }}
     >
-      <img
-        src={product.thumbnail}
-        alt={product.title}
-        style={{ width: "50%", height: "50%", borderRadius: "10px" }}
-      />
-    </div>
-    <h2>{product.title}</h2>
-    <p>{product.description}</p>
-    <p>Price: ৳{product.price}</p>
-    <button
-      onClick={onClose}
-      style={{
-        marginTop: "20px",
-        padding: "10px 20px",
-        cursor: "pointer",
-        backgroundColor: "#007bff",
-        color: "#fff",
-        border: "none",
-        borderRadius: "5px",
-        fontSize: "1em",
-        transition: "background-color 0.3s",
-      }}
-      onMouseOver={(e) => (e.target.style.backgroundColor = "#0056b3")}
-      onMouseOut={(e) => (e.target.style.backgroundColor = "#007bff")}
-    >
-      Close
-    </button>
-  </Modal>
-);
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#f0f0f0",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <img
+          src={product.thumbnail}
+          alt={product.title}
+          style={{ width: "50%", height: "50%", borderRadius: "10px" }}
+        />
+      </div>
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <div>
+        <span className="product-price">৳ {product.price}</span>
+        {oldPrice && <span className="product-old-price">৳ {oldPrice}</span>}
+      </div>
+      <button
+        onClick={onClose}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          cursor: "pointer",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          fontSize: "1em",
+          transition: "background-color 0.3s",
+        }}
+        onMouseOver={(e) => (e.target.style.backgroundColor = "#0056b3")}
+        onMouseOut={(e) => (e.target.style.backgroundColor = "#007bff")}
+      >
+        Close
+      </button>
+    </Modal>
+  );
+};
 
 export default ProductModal;
